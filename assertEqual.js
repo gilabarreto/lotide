cb9efd26cd33af9c50c32c720c3ea02e6422550f
Victor GB <gilabarreto@gmail.com>
2022-05-19 19:32:08 +0000

    Minor update

--- assertEqual.js
+++ assertEqual.js
@@ -9,6 +9,6 @@ const assertEqual = function(actual, expected) {
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("test", "test");
 assertEqual(1, 1);
 assertEqual(1, 2);
-assertEqual("teste", "teste");
