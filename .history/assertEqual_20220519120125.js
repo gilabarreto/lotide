127bef67f1fdf7c66cee151ba2726dddcc4a0f20
Victor GB <97627481+gilabarreto@users.noreply.github.com>
2022-05-22 15:36:13 -0600

    Delete .history directory

--- .history/assertEqual_20220519120125.js
+++
@@ -1,6 +0,0 @@
-const sum = function(a, b) {
-  return a + b;
-}
-
-console.assert(sum(1, 2) === 3);
-console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
--- .history/assertEqual_20220519121551.js
+++
@@ -1,8 +0,0 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  return actual === expected;
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
