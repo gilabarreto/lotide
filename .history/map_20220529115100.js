e0464302adeb2fb2f119a74841eaafa2804b2bd4
Victor Gila Barreto <gilabarreto@gmail.com>
2022-05-29 18:11:53 +0000

    Lotide Map Function

---
+++ .history/map_20220529115100.js
@@ -0,0 +1,14 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+
+  const results = [];
+
+  console.log('array: ', array);
+  console.log('callback: ', callback);
+
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
---
+++ .history/map_20220529121044.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function (array1, array2) {
+  for (let x = 0; x < array1.length; x++) {
+    if (array1.length !== array2.length || array1[x] !== array2[x]) {
+      return "🔴🔴🔴 Assertion Failed: The arrays are different.";
+    } else {
+      null;
+    }
+  }
+  return "🟢🟢🟢 Assertion Passed: The arrays are equal.";
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] === array2[x]) {
+        null;
+      } else {
+        return false;
+      }
+    }
+    return true
+  }
+  return false;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']));
