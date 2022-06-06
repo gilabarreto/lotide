f8ee75361c36201940abfe0bcedec679a4cf4e50
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-06 18:55:04 +0000

    removed history folder

--- .history/map_20220529114916.js
+++
@@ -1,8 +0,0 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
-const map = function() {
-  // empty for now :)
-}
-
-const results1 = map(words, word => word[0]);
-console.log(results1);
--- .history/map_20220529114953.js
+++
@@ -1,9 +0,0 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
-const map = function() {
-  const results = [];
-  return results;
-}
-
-const results1 = map(words, word => word[0]);
-console.log(results1);
