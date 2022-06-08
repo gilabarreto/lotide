ea7db47fba236732769d6bc292df3beac83d0945
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-08 22:17:36 +0000

    Minor changes

--- test/middleTest.js
+++ test/middleTest.js
@@ -10,6 +10,7 @@ console.log(middle([1, 2, 3, 4, 5])); // => [3]
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
+//TEST TO VERIFY IF THE FUNCTION IS CHANGING THE ARRAY
 const newArray = [1, 2, 3, 4, 5, 6]; 
 
 middle(newArray); // => [3, 4]
--- test/tailTest.js
+++ test/tailTest.js
@@ -8,6 +8,7 @@ console.log(assertEqual(tail(original).length, 2)); // ensure we get back two el
 console.log(assertEqual(tail(original)[0], "Lighthouse")); // ensure first element is "Lighthouse"
 console.log(assertEqual(tail(original)[1], "Labs")); // ensure second element is "Labs"
 
+//TEST TO VERIFY IF THE FUNCTION IS CHANGING THE ARRAY
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
