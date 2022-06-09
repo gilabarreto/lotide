f02fcf67345643b4e23a00f01a95c5019d9a74ea
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-09 01:49:16 +0000

    index shorthand update

--- index.js
+++ index.js
@@ -6,11 +6,4 @@ const middle = require('./middle');
 const tail = require('./tail');
 
 
-module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  head: head,
-  middle: middle,
-  tail: tail,
-};
+module.exports = { assertArraysEqual, assertEqual, eqArrays, head, middle, tail };
