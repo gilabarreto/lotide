26f2518ca5935f487c6acc543ed89e096a39d659
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-11 00:13:14 +0000

    index update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const eqArrays = require('./eqArrays');
 const head = require('./head');
 const middle = require('./middle');
 const tail = require('./tail');
+const countOnly = require('../countOnly');
 
-
-module.exports = { assertArraysEqual, assertEqual, eqArrays, head, middle, tail };
+module.exports = { assertArraysEqual, assertEqual, eqArrays, head, middle, tail, countOnly };
